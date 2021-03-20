de909c47e6b595a693d7dd4620411b3c94a9a85a
Tudor Roman <tudor.roman22@gmail.com>
2021-03-20 17:05:37 +0200

    fix await bug on connection

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import LedBulb from './iot/devices/xiaomi/ledBulb';
 dotenv.config({ path: `${__dirname}/../.env` });
 
 (async () => {
-  connect();
+  await connect();
 
   const hallLight = new LedBulb('0x00158d0004111517');
 
