af500e8e2cc3b9769e421a77978e076a7b98f9a4
Tudor Roman <tudor.roman22@gmail.com>
2021-03-20 17:00:55 +0200

    test connect function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import dotenv from 'dotenv';
-import mqttClient from './iot/mqtt';
+import { connect } from './iot/mqtt';
 import LedBulb from './iot/devices/xiaomi/ledBulb';
 
 dotenv.config({ path: `${__dirname}/../.env` });
 
 (async () => {
+  connect();
+
   const hallLight = new LedBulb('0x00158d0004111517');
 
   await hallLight.turnOn();
 
   setTimeout(() => {
     hallLight.turnOff();
-  }, 2000);
+  }, 4000);
 })();
--- src/iot/mqtt/index.js
+++ src/iot/mqtt/index.js
@@ -1,9 +1,19 @@
 import mqtt from 'async-mqtt';
 
-const client = mqtt.connect(process.env.MQTT_HOST);
+let client;
+
+const printDevices = () => {
+  client.publish(`${process.env.TOPIC_NAME}/bridge/devices`);
+};
 
 client.on('connect', () => {
   console.log('Connected to the Zigbee Network');
+
+  printDevices();
 });
+
+export const connect = () => {
+  client = mqtt.connect(process.env.MQTT_HOST);
+};
 
 export default client;
